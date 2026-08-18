d1e78f0217f5d10ee651203ef29a52bf888e6993
Arpit Sinha <mr.arpitsinha@gmail.com>
2026-08-18 11:02:56 +0530

    Refactor React setup with imports and element keys + Parcel docs

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello World"),
-    React.createElement("h2", {}, "Hello World"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "child1-h1" }, "Hello World"),
+    React.createElement("h2", { key: "child1-h2" }, "Hello World"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello World"),
-    React.createElement("h2", {}, "Hello World"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "child2-h1" }, "Hello World"),
+    React.createElement("h2", { key: "child2-h2" }, "Hello World"),
   ]),
 ]);
 //const heading = React.createElement("h1", { id: "heading" }, "Hello World");
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Basic React
+
+# Parcel
+
+- Dev build
+- Local server
+- HMR (Hot module replacement)
+- File watching algorithm (written in c++)
+- Faster builds because of caching (.parcel-cache)
+- Image optimization
+- Minification
+- Bundling
+- Compress the files
+- Consistent hashing
+- Code spliting
+- Differential bundling (support order browsers)
+- Diagnostics
+- Error handling
+- Supports to host on https
+- Tree shaking (remove unused code)
+- Different dev and prod bundles
